adfce7dfcebd07988565e4b30392f550238d9b31
46-debug <84792619+46-debug@users.noreply.github.com>
2024-02-16 12:12:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Weather from "./Weather"
+
+const App =()=> {
+  return (
+   <div className="main_div">
+    <Weather/>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/WeatherFunc.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+const WeatherFunc = () =>{
+    
+    
+
+};
+
+
+export default WeatherFunc;
